6316d97cbc5078a67e6151c2a17d7dc83681b7aa
Deneth123456789 <Deneth333333@gmail.com>
2025-08-20 08:46:18 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "deneth333333@gmail.com",
-  password: "deneth12047",
+  password: "Deneth2010",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,4 +41,5 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
 
